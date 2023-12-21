6fafcf559647f4ff6f15b884c687de7aa4224ce4
Samuel Nogueira <samuelnbacelar@hotmail.com>
2023-12-21 09:36:20 -0300

    add rota de obter todos analytics

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -9,4 +9,8 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+  @Get('/analytics')
+  getAnalytics() {
+    return this.appService.getAnalytics();
+  }
 }
--- back/src/app.service.ts
+++ back/src/app.service.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@nestjs/common';
-
+const data = [
+  {
+    '2023-12-01': {
+      'page-views': 30,
+      'clicked-links': {
+        'link-01': 20,
+        'link-02': 5,
+        'link-03': 5,
+      },
+    },
+  },
+];
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+
+  getAnalytics() {
+    return data;
   }
 }
